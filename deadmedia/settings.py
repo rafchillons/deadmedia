debf2926d32fc291de98b2a65662ba348b92bf50
root <root@ns329463.ip-37-187-116.eu>
2017-11-02 17:51:58 +0000

    first commit from srver

--- deadmedia/settings.py
+++ deadmedia/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'mod_wsgi.server',
+    'mod_wsgi.server',
     'deadsource',
 ]
 
