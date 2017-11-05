ca924f0cbce3efe26c167598595f3d83245b4cd2
root <root@ns329463.ip-37-187-116.eu>
2017-11-05 15:53:24 +0000

    random sign-in images javascript + sign.html fixies

--- deadsource/static/js/style.js
+++ deadsource/static/js/style.js
@@ -17,11 +17,11 @@ $(document).ready(function () {
 
 
 var infinite = new Waypoint.Infinite({
-element: $('.infinite-container')[0],
-onBeforePageLoad: function () {
-    $('.loading').show();
-},
-onAfterPageLoad: function ($items) {
-    $('.loading').hide();
-}
+    element: $('.infinite-container')[0],
+    onBeforePageLoad: function () {
+        $('.loading').show();
+    },
+    onAfterPageLoad: function ($items) {
+        $('.loading').hide();
+    }
 });
