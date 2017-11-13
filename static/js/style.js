5a04a32e089bacfa0d53eb00664405876b0af226
mrcandy <mindMaester@gmail.com>
2017-11-13 18:10:29 +0300

    merge done

--- static/js/style.js
+++ static/js/style.js
@@ -14,14 +14,3 @@ $(document).ready(function () {
         }
     });
 });
-
-
-var infinite = new Waypoint.Infinite({
-element: $('.infinite-container')[0],
-onBeforePageLoad: function () {
-    $('.loading').show();
-},
-onAfterPageLoad: function ($items) {
-    $('.loading').hide();
-}
-});
