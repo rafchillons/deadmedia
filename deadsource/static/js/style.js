6f365f9b25a427833c93419d357e1e043042cf42
root <root@ns329463.ip-37-187-116.eu>
2017-11-14 19:24:38 +0000

    new admin panel

--- deadsource/static/js/style.js
+++ deadsource/static/js/style.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-    //tip
-    tippy('.category-title > .title');
-
     //reesponsive menu
     $('#menu-trigger-btn').click(function () {
         $('.menu ul').slideToggle(500);
@@ -13,15 +10,9 @@ $(document).ready(function () {
             $('.menu ul').removeAttr('style');
         }
     });
-});
 
-
-var infinite = new Waypoint.Infinite({
-    element: $('.infinite-container')[0],
-    onBeforePageLoad: function () {
-        $('.loading').show();
-    },
-    onAfterPageLoad: function ($items) {
-        $('.loading').hide();
-    }
+    //admin special information button 
+    $('body').on('click', '.file-admin-information-btn', function () {
+        $(this).parent().find('.file-admin-information').slideToggle();
+    });
 });
