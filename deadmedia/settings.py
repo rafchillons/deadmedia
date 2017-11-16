694aa4151dd4787c9a66cd54669f25343f2028f5
root <root@ns329463.ip-37-187-116.eu>
2017-11-16 19:55:12 +0000

    logs deleted

--- deadmedia/settings.py
+++ deadmedia/settings.py
@@ -106,24 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'logs.txt',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+    
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
