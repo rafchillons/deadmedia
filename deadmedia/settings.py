10c8c605960681c57c623d706766fe78d32be57f
mrcandy <mindMaester@gmail.com>
2017-12-25 02:12:52 +0300

    settings added

--- deadmedia/settings.py
+++ deadmedia/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'deadmedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dead_db',
+        'USER': 'dbuser',
+        'PASSWORD': 'abacaba',
+        #'HOST': 'dbprod01ned.mycompany.com',
+        #'PORT': '1540',
     }
 }
 
